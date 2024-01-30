0cfde8f667653a075503089762caa79d825e097a
Afnan031 <afnanmemon28@gmail.com>
2024-01-30 20:38:03 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,170 @@
+// Question.1 
+    //                          Answer
+    
+    // alert('Welcome to Our Website')
+
+
+// Question.2 
+    //                          Answer
+
+    // console.log('Please Enter a Valid Password')
+
+
+// Question.3 
+    //                          Answer
+
+    // alert('Welcome to our  Website \n see my Website')
+
+
+// Question.4 
+    //                          Answer
+
+    // alert(' First Alert Message')
+    // alert(' Second Alert Message')
+    // alert(' Third Alert Message')
+
+
+// Question.5 
+    //                          Answer
+    // alert('Hello')
+
+
+// Question.6 
+        //                          Answer
+
+        // c. Body[inside your page's HTML]
+
+
+// Q.7 
+    //                          Answer
+
+    // var username
+
+
+// Question.8 
+    //                          Answer
+
+    // var myName = 'Afnan'
+    // console.log(myName)
+
+
+// Question.9 
+        //                          Answer
+
+        // var titledMessage = 'Hello World'
+        // alert(titledMessage)
+
+
+// Question.10 
+    //                          Answer
+
+    // var alert1 = '1'
+    // var alert2 = '2'
+    // var alert3 = '3'
+    // alert(alert1)
+    // alert(alert2)
+    // alert(alert3)
+
+
+// Question.11 
+    //                          Answer
+
+    // var email = 'afnanmemon28@gmail.com'
+    // alert('Email: '+ email)
+
+
+// Question.12 
+    //                          Answer
+
+    // See Q.'13' for the Answer
+
+
+// Question.13 
+    //                          Answer
+
+    // var string = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬'
+    // alert(string)
+    // Answer No.12
+    // console.log(string)
+
+
+// Question.14 
+    //                          Answer
+
+    // var age = 20
+    // alert('Age: '+age)
+
+
+// Question.15 
+    //                          Answer
+
+    // var visit = 0
+    // alert('You Have Visited this Site ' + visit++ + ' Number of Times.')
+
+
+// Question.16 
+    //                          Answer
+
+    // var birthYear = 2003
+    // console.log('Birth Year: '+birthYear)
+
+
+// Question.17
+    //                          Answer
+
+    // var Name = 'Afnan',
+    // product = 'TV',
+    // quantity = '5',
+    // store = 'Gada Electronics'
+    // alert(Name+' Purchase '+quantity + product+' from '+store)
+
+
+// Question.18 
+    //                          Answer
+
+    // var myFullName = 'Afnan', myCourse = 'Web Development #1', myLocation = 'Bano Qabil 2.0-'
+
+
+// Question.19  Declare 5 legal & 5 illegal variable names
+    //                          Answer
+
+
+// Question.20 
+    //                          Answer
+
+
+// Question.21 
+    //                          Answer
+
+    // var program_1 = 10
+    // var program_2 = 5
+    // var program = program_1 + program_2
+    // alert('Sum of two Numbers: '+program)
+
+
+// Question.22 
+    //                          Answer
+
+    // For Substraction
+    // var program_1 = 20
+    // var program_2 = 5
+    // var program = program_1 - program_2
+    // alert('Substraction of two Numbers: '+program)
+
+    // For Multiplication
+    // var program_1 = 20
+    // var program_2 = 5
+    // var program = program_1 * program_2
+    // alert('Multiplication of two Numbers: '+program)
+
+    // For Division
+    // var program_1 = 10
+    // var program_2 = 5
+    // var program = program_1 / program_2
+    // alert('Division of two Numbers: '+program)
+
+    // For Modulus
+    // var program_1 = 20
+    // var program_2 = 5
+    // var program = program_1 % program_2
+    // alert('Modulus of two Numbers: '+program)
